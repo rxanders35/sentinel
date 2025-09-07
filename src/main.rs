d76ec4f3b462f90f32ed35b85b626857b6c6bd12
rxanders35 <reidxanders@outlook.com>
2025-09-07 01:38:14 -0400

    basic CLI started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    // mode of a sidecar
+    #[arg(long, value_enum)]
+    mode: Option<Mode>,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum Mode {
+    Sender,
+    Receiver,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.mode {
+        Some(Mode::Sender) => println!("I am a Sender"),
+        Some(Mode::Receiver) => println!("I am a Receiver"),
+        None => println!("You need to provide a mode"),
+    }
 }
